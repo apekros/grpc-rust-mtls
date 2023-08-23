a59623ce3ed74d818a510ab07f4c01becbe0368f
apekros <bayleyfoster@gmail.com>
2023-08-23 16:29:45 +0930

    Add tls for service and health check test

--- src/health_tests.rs
+++ src/health_tests.rs
@@ -1,6 +1,6 @@
 use crate::start_service;
 use tokio::task;
-
+use tonic::transport::{Certificate, ClientTlsConfig};
 use tonic_health::pb::health_client::HealthClient;
 use tonic_health::pb::HealthCheckRequest;
 
@@ -8,7 +8,17 @@ use tonic_health::pb::HealthCheckRequest;
 async fn test_health_service() {
     task::spawn(start_service());
 
-    let conn = tonic::transport::Endpoint::from_static("http://localhost:8080")
+    let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "tls"]);
+    let server_root_ca_cert = std::fs::read_to_string(data_dir.join("ca.pem")).unwrap();
+    let server_root_ca_cert = Certificate::from_pem(server_root_ca_cert);
+
+    let tls = ClientTlsConfig::new()
+        .domain_name("localhost")
+        .ca_certificate(server_root_ca_cert);
+
+    let conn = tonic::transport::Endpoint::from_static("https://localhost:8080")
+        .tls_config(tls)
+        .unwrap()
         .connect()
         .await
         .unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[cfg(test)]
 mod health_tests;
 
 use example::{
@@ -5,6 +6,7 @@ use example::{
     HelloRequest, HelloResponse,
 };
 
+use tonic::transport::{Identity, ServerTlsConfig};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod example {
@@ -26,6 +28,7 @@ impl Example for ExampleService {
         }))
     }
 }
+
 pub async fn start_service() {
     let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
 
@@ -36,12 +39,21 @@ pub async fn start_service() {
     let address = "[::1]:8080".parse().unwrap();
     let example_service = ExampleService::default();
 
+    let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "tls"]);
+    let cert = std::fs::read_to_string(data_dir.join("server.pem")).unwrap();
+    let key = std::fs::read_to_string(data_dir.join("server.key")).unwrap();
+    let server_identity = Identity::from_pem(cert, key);
+
+    let tls = ServerTlsConfig::new().identity(server_identity);
+
     Server::builder()
+        .tls_config(tls)
+        .unwrap()
         .add_service(ExampleServer::new(example_service))
         .add_service(health_service)
         .serve(address)
         .await
-        .expect("STart service failed.");
+        .expect("Start service failed.");
 }
 
 #[tokio::main]
